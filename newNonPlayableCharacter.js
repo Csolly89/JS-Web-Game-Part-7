f8052ac59b43afde6faba86760dd66b7157e6d25
Csolly89 <141281525+Csolly89@users.noreply.github.com>
2023-09-15 22:35:54 -0400

    circuit complete

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -29,26 +29,32 @@ function newNonPlayableCharacter(x, y) {
     // Use await to pause walkEast until sleep (time) has resolved.
     // After sleep has resolved, invoke stop to stop the character.
 
-  async  function walkEast(time) {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
         await sleep(time)
         stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
     function stop() {
